53b5ecfab599f0601eab69180f9886b42d22bfc8
Reiner Gerecke <me@reinergerecke.de>
2020-04-07 07:04:52 +0200

    Generalize implementation
    Don't have enough tests yet to confirm it, but this should work. I'm
    inspecting the dataclasses, ignoring things like whitespace and map that
    to corresponding instances of matcher classes.

--- libcst_easy_matcher/__init__.py
+++ libcst_easy_matcher/__init__.py
@@ -1,3 +1,6 @@
+import dataclasses
+from collections.abc import Iterable
+
 import libcst
 import libcst.matchers as m
 
@@ -14,50 +17,32 @@ class Transformer:
     """
 
     def on_visit(self, node):
-        visit_func = getattr(self, f"visit_{type(node).__name__}", None)
-        if not visit_func:
-            raise NotImplementedError(
-                f"Implementation for '{type(node).__name__}' missing."
-            )
-        return visit_func(node)
+        name = type(node).__name__
 
-    def visit_Assign(self, node):
-        return m.Assign(
-            targets=[self.on_visit(t) for t in node.targets],
-            value=self.on_visit(node.value),
-        )
-
-    def visit_AssignTarget(self, node):
-        return m.AssignTarget(target=self.on_visit(node.target))
-
-    def visit_Name(self, node):
-        if node.value == "__":
+        if name == 'Name' and node.value == '__':
             return m.DoNotCare()
-        else:
-            return m.Name(value=node.value)
 
-    def visit_SimpleString(self, node):
-        return m.SimpleString(value=node.value)
+        kwargs = {}
+        for field in dataclasses.fields(node):
+            if field.name in ['semicolon', 'comma', 'equal']:
+                continue
+            if 'whitespace' in field.name:
+                continue
 
-    def visit_Integer(self, node):
-        return m.Integer(value=node.value)
+            value = getattr(node, field.name)
+            if value is None:
+                continue
 
-    def visit_Call(self, node):
-        return m.Call(
-            func=self.on_visit(node.func), args=[self.on_visit(a) for a in node.args]
-        )
+            if isinstance(value, str):
+                pass
+            elif isinstance(value, Iterable):
+                value = [self.on_visit(e) for e in value]
+            else:
+                value = self.on_visit(value)
 
-    def visit_Expr(self, node):
-        return m.Expr(value=self.on_visit(node.value))
-    
-    def visit_Arg(self, node):
-        if node.keyword is None:
-            return m.DoNotCare()
-        else:
-            return m.Arg(
-                value=self.on_visit(node.value),
-                keyword=self.on_visit(node.keyword)
-            )
+            kwargs[field.name] = value
+        
+        return getattr(m, name)(**kwargs)
 
 
 def create_matcher(code):
